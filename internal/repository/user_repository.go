c3bf5a189071db3763c82eb86cba8a9c149ab463
octaview <kprinzin@gmail.com>
2025-04-11 20:51:06 +0300

    added UserRepository with methods for creating a user and finding by email

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"context"
+	"errors"
+
+	"kanban/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create(ctx context.Context, user *model.User) error {
+	return r.db.WithContext(ctx).Create(user).Error
+}
+
+func (r *UserRepository) FindByEmail(ctx context.Context, email string) (*model.User, error) {
+	var user model.User
+	err := r.db.WithContext(ctx).Where("email = ?", email).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	return &user, err
+}
